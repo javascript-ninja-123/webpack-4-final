ad429ba36f53431343617ebcb1374d3472099242
Sung Yi <Sung.Yi@fox.com>
2019-01-10 11:10:08 -0800

    s

--- webpack-development.js
+++ webpack-development.js
@@ -21,7 +21,9 @@ module.exports = {
                   options: {
                     presets: ['@babel/preset-env',"@babel/preset-react"],
                     plugins:["@babel/plugin-proposal-object-rest-spread","@babel/plugin-transform-runtime",
-                    "babel-plugin-styled-components","@babel/plugin-proposal-class-properties"
+                    "babel-plugin-styled-components",
+                    [ "@babel/plugin-proposal-decorators", {"legacy":true}],
+                    "@babel/plugin-proposal-class-properties"
                     ]
                   }
                 }

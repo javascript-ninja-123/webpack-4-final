c22dea42362f046cc7e1cd4f9ada4a60c9427d0d
Sung Yi <Sung.Yi@fox.com>
2019-01-14 11:12:17 -0800

    fixed

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import './style/index.less';
 import './registerServiceWorker';
+import App from './app';
 
 
 
 
 
-
+ReactDOM.render(<App/>, document.getElementById('root'));
